2dbfa838b32d807e2da41b3b371ba49e01ab3841
Chase Brooks <meovermymind@gmail.com>
2022-11-04 18:25:52 -0400

    work on Character

--- src/eatyodaeat/Character.java
+++ src/eatyodaeat/Character.java
@@ -6,23 +6,57 @@ import java.util.Map;
 
 public class Character extends JButton {
     private static Map<String,Icon> imageMap = new HashMap<>();
-  "yoda"    Icon for yoda
-  "eggs"     Icon for eggs
-
     private String name;  // "yoda", "eggs", "trooper", "vader"
     private boolean isClicked;
+    private Icon icon;
 
-    public CharacterButton(String name) {
-        setIconImage(imageMap.get(name));
+    public Character(String name,Icon icon, boolean isClicked) {
+
     }
 
-    public boolean matches(CharacterButton other) {
+    public boolean matches(Character other) {
         return this.name.equals(other.name);
     }
 
     // maybe not
     public boolean isDarthVader() {
         return this.name.equals("vader");
+    }
+
+    public static Map<String, Icon> getImageMap() {
+        return imageMap;
+    }
+
+    public static void setImageMap(Map<String, Icon> imageMap) {
+        Character.imageMap = imageMap;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isClicked() {
+        return isClicked;
+    }
+
+    public void setClicked(boolean clicked) {
+        isClicked = clicked;
+    }
+
+    @Override
+    public Icon getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setIcon(Icon icon) {
+        this.icon = icon;
     }
 
     static {
