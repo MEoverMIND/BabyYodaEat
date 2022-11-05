ba17769f4f5c6f0678993e2dac4219e7528c0ee8
Chase Brooks <meovermymind@gmail.com>
2022-11-05 18:39:59 -0400

    Changed Board class. it no longer extends Jframe. it now uses a Jframe

--- src/com/eatyodaeatclient/PlayerClient.java
+++ src/com/eatyodaeatclient/PlayerClient.java
@@ -5,9 +5,11 @@ package com.eatyodaeatclient;
  * Instantiate the controller and say go
  */
 
+import com.eatyodaeat.Board;
+
 class PlayerClient {
     public static void main(String[] args) {
+        Board frame = new Board();
 
     }
-
 }
