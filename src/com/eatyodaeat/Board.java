7eef8638e29a330c3594c49cf141503c681af0fb
Chase Brooks <meovermymind@gmail.com>
2022-11-05 14:07:18 -0400

    I worked on Board and Character. I added the setters to the CTOR, but wondered if we should consider a CTOR factory. I think this could be useful if we plan on adding more character options. I also updated Board to use the character class. I also added a turn tracker field and a start button. We may need classes for each.

--- src/com/eatyodaeat/Board.java
+++ src/com/eatyodaeat/Board.java
@@ -14,9 +14,11 @@ public class Board extends JFrame {
         // Jpanel is a container that stores the components of the GUI
         JPanel container = new JPanel();
 
+
         // Set the grid, starting with 9
         GridLayout grid = new GridLayout(3, 3, 10, 10);
         container.setLayout(grid);
+
 
         Icon yodaImage = new ImageIcon("images/baby_yoda.jpeg");
         Icon c3p0Image = new ImageIcon("images/c3p0.jpg");
@@ -25,34 +27,51 @@ public class Board extends JFrame {
         Icon vaderImage = new ImageIcon("/Users/shimidolo/Desktop/pixle art/darth_vader.png");
         Icon foodImage = new ImageIcon("/Users/shimidolo/Desktop/pixle art/macaroon.jpeg");
 
-        JButton a = new JButton(foodImage);
-        JButton b = new JButton(c3p0Image);
-        JButton c = new JButton(mandoImage);
-        JButton d = new JButton(stormTropperImage);
-        JButton e = new JButton(vaderImage);
-        JButton f = new JButton(stormTropperImage);
-        JButton g = new JButton(mandoImage);
-        JButton h = new JButton(c3p0Image);
-        JButton i = new JButton(yodaImage);
+        JProgressBar turn = new JProgressBar();
+        JButton start = new JButton();
+        JButton character1 = new Character("yoda", yodaImage,false);
+        JButton character2 = new Character("c3p0", c3p0Image,false);
+        JButton character3 = new Character("mando", mandoImage,false);
+        JButton character4 = new Character("stormy", stormTropperImage,false);
+        JButton character5 = new Character("vader", vaderImage,false);
+        JButton character6 = new Character("food", foodImage,false);
+        JButton character2_2 = new Character("c3p0", c3p0Image,false);
+        JButton character3_2 = new Character("mando", mandoImage,false);
+        JButton character4_2 = new Character("stormy", stormTropperImage,false);
+
+          // commented out all Jbutton Intialization
+
+//        JButton a = new JButton(foodImage);
+//        JButton b = new JButton(c3p0Image);
+//        JButton c = new JButton(mandoImage);
+//        JButton d = new JButton(stormTropperImage);
+//        JButton e = new JButton(vaderImage);
+//        JButton f = new JButton(stormTropperImage);
+//        JButton g = new JButton(mandoImage);
+//        JButton h = new JButton(c3p0Image);
+//        JButton i = new JButton(yodaImage);
 
 
+        // added and Turn tracker to JFrame
+        add( new JPanel(){{ add(start);}}, BorderLayout.SOUTH);
+        add( new JPanel(){{ add(turn);}}, BorderLayout.NORTH);
 
-
-        container.add(a);
-        container.add(b);
-        container.add(c);
-        container.add(d);
-        container.add(e);
-        container.add(f);
-        container.add(g);
-        container.add(h);
-        container.add(i);
+        container.add(character1);
+        container.add(character2);
+        container.add(character3);
+        container.add(character4);
+        container.add(character5);
+        container.add(character6);
+        container.add(character2_2);
+        container.add(character3_2);
+        container.add(character4_2);
         add(container);
         setVisible(true);
         setResizable(false);
 
-
     }
+
+    // main class for testing
 
     public static void main(String[] arguments) {
         Board frame = new Board();
