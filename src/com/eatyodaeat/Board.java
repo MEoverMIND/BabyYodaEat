4718e81864759edfce533b17d2899459bf82b417
Chase Brooks <meovermymind@gmail.com>
2022-11-06 18:08:37 -0500

    Changed Board class. it no longer extends Jframe. it now uses a Jframe

--- src/com/eatyodaeat/Board.java
+++ src/com/eatyodaeat/Board.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import javax.swing.*;
 
-public class Board{
+public class Board implements ActionListener{
 
     // TODO
     /*
@@ -25,15 +25,31 @@ public class Board{
     // step 5 - when the player selects two card. Loop through each card and run card methods
 
 
-    List<JButton> cards = new ArrayList<>();
+    List<Character> cards = new ArrayList<>();
     JFrame board = new JFrame();
     // JPanel startField = new JPanel();
     // JPanel turnField = new JPanel();
     JPanel cardLayout = new JPanel();
     // JPanel is a container that stores the components of the GUI
     GridLayout grid = new GridLayout(3, 3, 10, 10);
+    // Load images into Icons
+    Icon yodaImage = new ImageIcon("images/baby_yoda.jpeg");
+    Icon c3p0Image = new ImageIcon("images/c3p0.jpg");
+    Icon mandoImage = new ImageIcon("images/mando.png");
+    Icon stormTropperImage = new ImageIcon("images/storm_tropper.jpeg");
+    Icon vaderImage = new ImageIcon("images/darth_vader.png");
+    Icon foodImage = new ImageIcon("images/macaroon.jpeg");
+    Icon backCard = new ImageIcon("images/white_back");
 
 
+    @Override
+    public void actionPerformed(ActionEvent e){
+        for (int i = 0; i < 9; i++) {
+            if (e.getSource() == cards.get(i)) {
+                cards.get(i).setIcon(cards.get(i).getBack());
+            }
+        }
+    }
 
     public Board() {
         board.setTitle("Yoda Board");
@@ -43,29 +59,24 @@ public class Board{
         // Set the grid, starting with 9
         cardLayout.setLayout(grid);
 
-        // Load images into Icons
-        Icon yodaImage = new ImageIcon("images/baby_yoda.jpeg");
-        Icon c3p0Image = new ImageIcon("images/c3p0.jpg");
-        Icon mandoImage = new ImageIcon("/Users/shimidolo/Desktop/pixle art/mando.png");
-        Icon stormTropperImage = new ImageIcon("/Users/shimidolo/Desktop/pixle art/storm_tropper.jpeg");
-        Icon vaderImage = new ImageIcon("/Users/shimidolo/Desktop/pixle art/darth_vader.png");
-        Icon foodImage = new ImageIcon("/Users/shimidolo/Desktop/pixle art/macaroon.jpeg");
-        Icon backCard = new ImageIcon();
+
+
+
 
         // Start and Turn buttons
         JProgressBar turn = new JProgressBar();
         JButton start = new JButton();
 
         // Create the JButtons to put into the JPanel
-        JButton character1 = new Character("yoda", yodaImage, false,backCard );
-        JButton character2 = new Character("c3p0", c3p0Image, false,backCard);
-        JButton character3 = new Character("mando", mandoImage, false,backCard);
-        JButton character4 = new Character("stormy", stormTropperImage, false,backCard);
-        JButton character5 = new Character("vader", vaderImage, false,backCard);
-        JButton character6 = new Character("food", foodImage, false,backCard);
-        JButton character2_2 = new Character("c3p0", c3p0Image, false,backCard);
-        JButton character3_2 = new Character("mando", mandoImage, false,backCard);
-        JButton character4_2 = new Character("stormy", stormTropperImage, false,backCard);
+        Character character1 = new Character("yoda", yodaImage, false,backCard );
+        Character character2 = new Character("c3p0", c3p0Image, false,backCard);
+        Character character3 = new Character("mando", mandoImage, false,backCard);
+        Character character4 = new Character("stormy", stormTropperImage, false,backCard);
+        Character character5 = new Character("vader", vaderImage, false,backCard);
+        Character character6 = new Character("food", foodImage, false,backCard);
+        Character character2_2 = new Character("c3p0", c3p0Image, false,backCard);
+        Character character3_2 = new Character("mando", mandoImage, false,backCard);
+        Character character4_2 = new Character("stormy", stormTropperImage, false,backCard);
 
         // Put each card into the "cards" ArrayList
         cards.add(character1);
@@ -84,6 +95,8 @@ public class Board{
         // For loop to place the Jbuttons on the panel
         for (int i = 0; i < 9; i++) {
             cardLayout.add(cards.get(i));
+            cards.get(i).addActionListener(this);
+            cards.get(i).setIcon(backCard);
 
         }
 
@@ -103,6 +116,8 @@ public class Board{
         board.setResizable(false);
 
 
+
+
 //        // add how many Tries it took the winner to win
 //        int tryCount = 0;
 //        // increment tryCount
@@ -118,6 +133,10 @@ public class Board{
 //
 //        // if you lose "select vader" you will have to answer a question
 //        relate to java for an extra turn
+
+
+
+
    }
 
 
@@ -127,3 +146,4 @@ public class Board{
         Board frame = new Board();
     }
 }
+
