27e7460bf94f57039369c66b2383e895a3844e25
Veronica Pichay <veronicapichay@outlook.com>
2022-11-07 08:47:14 -0800

    Controller workflow

--- src/com/eatyodaeat/app/BabyYodaEatApp.java
+++ src/com/eatyodaeat/app/BabyYodaEatApp.java
@@ -1,5 +1,11 @@
 package com.eatyodaeat.app;
 
+/*
+ * Application Controller
+ * Directs overall flow of the application
+ * Prompts user for inputs and forwards those inputs into the system (model) classes
+ */
+
 class BabyYodaEatApp {
 
 
