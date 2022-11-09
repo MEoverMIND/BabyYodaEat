1ed4c6177f7fd7e07fa2133741d8bce3ac4a87c3
Veronica Pichay <veronicapichay@outlook.com>
2022-11-09 11:46:50 -0800

    Changed audio file

--- src/com/eatyodaeat/model/Board.java
+++ src/com/eatyodaeat/model/Board.java
@@ -1,182 +1,154 @@
 package com.eatyodaeat.model;
 
+import com.eatyodaeat.model.Card;
+
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.*;
 import java.util.List;
 
-class Board implements ActionListener {
+// class BoardLogicTest implements ActionListener {
 
+public class Board extends JFrame  {
 
-    List<Character> cards = new ArrayList<>();
-    List<Character> cardsClicked = new ArrayList<>();
-    Set<String> stringMatch = new HashSet<>();
-    int numClicks = 0;
-    int matches = 0;
-    static JFrame board = new JFrame();
-    JPanel cardLayout = new JPanel();
-    boolean lost = false;
+    private List<Card> Deck = new ArrayList<>();
+    private List<Card> Catcher = new ArrayList<>();
+    private List<Card> flipBack = new ArrayList<>();
+    private Timer timer = null;
+    private boolean isCleared = false;
 
-    // JPanel is a container that stores the components of the GUI
-    GridLayout yodaGrid = new GridLayout(3, 3, 10, 10);
+    Board() {
+    }
 
-    // Load images into Icons
-    Icon yodaImage = new ImageIcon("images/baby_yoda.jpeg");
-    Icon c3p0Image = new ImageIcon("images/c3p0.jpg");
-    Icon mandoImage = new ImageIcon("images/mando.png");
-    Icon stormTropperImage = new ImageIcon("images/storm_tropper.jpeg");
-    Icon vaderImage = new ImageIcon("images/darth_vader.png");
-    Icon foodImage = new ImageIcon("images/macaroon.jpeg");
-    Icon backCard = new ImageIcon("images/white_back");
+    public void initUI() {
 
-    // TODO logic
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // number of cards clicked
-        numClicks++;
-        stringMatch.add(e.getActionCommand());
+        this.setTitle("Yoda Board");
+        this.setSize(850, 850);
+        this.setLayout(new GridLayout(3, 3, 10, 10));
+        this.setLocationRelativeTo(null);
 
-        System.out.println(stringMatch);
+        creatCard();
+        loadCard();
+        flipCard();
 
-        for (int i = 0; i < 9; i++) {
-//            System.out.println(numClicks);
-            if (e.getSource() instanceof Character) {
-                cards.get(i).turnOver();
-//                System.out.println(e.getActionCommand());
-                cardsClicked.add((Character) e.getSource());
+        this.setVisible(true);
+    }
 
-
-//                System.out.println(numClicks);
-//                System.out.println(cardsClicked);
-            }
-            while (numClicks == 2) {
-
-
-
-                // will be game over
-                if (vaderMatches()){
-                    System.out.println("welcome to the darkside");
-                    stringMatch.clear();
-                    lost = true;
-                    numClicks = 0;
-                }
-
-                // will be game over
-                if (yodaFed()){
-                    System.out.println("YUMMY!");
-                    stringMatch.clear();
-                    lost = true;
-                    numClicks = 0;
-                }
-
-                if (stringMatch.size() == 1) {
-                    System.out.println("you win");
-                    matches++;
-                    stringMatch.clear();
-                    numClicks = 0;
-
-                }
-
-                if (stringMatch.size() >= 2) {
-
-                    System.out.println("you lose");
-                    stringMatch.clear();
-                    System.out.println(stringMatch);
-                    lost = true;
-                    numClicks = 0;
-                }
+    public void creatCard() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j <= 5; j++) {
+                Icon Face = new ImageIcon("images/" + i + "Character.jpeg");
+                Icon Back = new ImageIcon("images/white_back.jpeg");
+                Card card = new Card(Face, Back, false, i, false, false);
+                card.setIcon(Back);
+                Deck.add(card);
             }
         }
+        Collections.shuffle(Deck);
     }
 
-    public Board() {
-        board.setTitle("Yoda Board");
-        board.setSize(850, 850);
-        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Set the grid, starting with 9
-        cardLayout.setLayout(yodaGrid);
-
-
-        // Create the JButtons to put into the JPanel
-        Character character1 = new Character("yodaFed", yodaImage, false, backCard);
-        Character character2 = new Character("c3p0", c3p0Image, false, backCard);
-        Character character3 = new Character("mando", mandoImage, false, backCard);
-        Character character4 = new Character("stormy", stormTropperImage, false, backCard);
-        Character character5 = new Character("vader", vaderImage, false, backCard);
-        Character character6 = new Character("yodaFed", foodImage, false, backCard);
-        Character character2_2 = new Character("c3p0", c3p0Image, false, backCard);
-        Character character3_2 = new Character("mando", mandoImage, false, backCard);
-        Character character4_2 = new Character("stormy", stormTropperImage, false, backCard);
-
-        // Put each card into the "cards" ArrayList
-        cards.add(character1);
-        cards.add(character2);
-        cards.add(character3);
-        cards.add(character4);
-        cards.add(character5);
-        cards.add(character6);
-        cards.add(character2_2);
-        cards.add(character3_2);
-        cards.add(character4_2);
-
-        // this method shuffles the ArrayList to randomize the Panel
-        Collections.shuffle(cards);
-
-        // For loop to place the Jbuttons on the panel
-        placeButtons();
-
-        // assigning layout to board
-        board.add(cardLayout);
-        board.setVisible(true);
-        board.setResizable(false);
-
-
-//        // added and Start button to JFrame
-//        board.add(new JPanel() {{
-//            add(start);
-//        }}, BorderLayout.SOUTH);
-//
-//        // added and Turn tracker to JFrame
-//        board.add(new JPanel() {{
-//            add(turn);
-//        }}, BorderLayout.NORTH);
-
-
-    }
-
-    private void placeButtons() {
-        for (int i = 0; i < 9; i++) {
-            cardLayout.add(cards.get(i));
-            cards.get(i).addActionListener(this);
-            cards.get(i).setIcon(backCard);
-
+    public void loadCard() {
+        for (Card card : Deck) {
+            this.add(card);
         }
     }
 
-    public boolean vaderMatches() {
-        return stringMatch.stream()
-                .anyMatch(String -> String.equals("vader"));
-    }
+    public void flipCard() {
+        for (Card card : Deck) {
+            card.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (!card.cantClick) {
+                        if (card.isFaceUp) {
+                            card.setIcon(card.BackSide);
+                            card.isFaceUp = false;
+                        } else {
+                            card.setIcon(card.FaceSide);
+                            card.isFaceUp = true;
+                            Catcher.add(card);
+                        }
+                        card.cantClick = true;
+                    }
+                    setStatus();
 
-    public boolean yodaFed() {
-        return (stringMatch.stream().anyMatch(String -> String.equals("yodaFed")) && stringMatch.size() == 1);
-    }
+                    // check end game
+                    for (Card card : Deck) {
+                        if (!card.isFaceUp) {
+                            isCleared = false;
+                            break;
+                        } else {
+                            isCleared = true;
+                        }
+                    }
+                    checkEndGame();
+                }
 
+                @Override
+                public void mousePressed(MouseEvent e) {
+                }
 
-    public void reset() {
-        board.repaint();
-        Board board = new Board();
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                }
 
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                }
+            });
         }
+    }
 
-
-    // main class for testing
-
-    public static void main(String[] arguments) {
-        Board board = new Board();
-
+    public void setStatus() {
+        int delay = 800; //milliseconds
+        if (Catcher.size() == 2) {
+            if (Catcher.get(0).matched(Catcher.get(1))) {
+                for (Card card : Deck) {
+                    if (card.isFaceUp) {
+                        card.isMatched = true;
+                        card.cantClick = true;
+                    }
+                }
+            } else {
+                flipBack.addAll(Catcher);
+                ActionListener taskPerformer = new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        for (Card card : flipBack) {
+                            if (!card.isMatched) {
+                                card.setIcon(card.BackSide);
+                                card.isFaceUp = false;
+                                card.cantClick = false;
+                            }
+                        }
+                        flipBack.clear();
+                    }
+                };
+                if (timer != null) timer.stop();
+                timer = new Timer(delay, taskPerformer);
+                timer.start();
+            }
+            Catcher.removeAll(Catcher);
+        }
+    }
+    private void checkEndGame() {
+        if (isCleared) {
+            int out = JOptionPane.showConfirmDialog(this, "Play", "Goodbye", JOptionPane.YES_NO_OPTION);
+            if (out == JOptionPane.YES_OPTION) {
+                this.setVisible(false);
+                Board board = new Board();
+                board.initUI();
+            } else if (out == JOptionPane.NO_OPTION) {
+                this.setVisible(false);
+            }
+        }
     }
 }
+
